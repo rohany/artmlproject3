3044b5c88ac06d9de5a92c80e3d1b6cfdd9cb98c
Rohan Yadav <rohany@andrew.cmu.edu>
2019-03-20 16:13:18 -0400

    first commit

---
+++ process-kaggle-data.py
@@ -0,0 +1,21 @@
+import csv
+import sys
+
+s = set()
+selected = []
+with open(sys.argv[1], 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        if row[4] == 'Hip-Hop':
+            selected.append(row)
+
+print(len(selected))
+
+tot = 0
+for row in selected:
+    lyrics = row[-1]
+    x = lyrics.split(" ")
+    charcount = sum([len(x) for x in x])
+    tot += charcount
+
+print(tot)
